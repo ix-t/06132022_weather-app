2f62e20728f31c0a2095b95fb39e6e31e5f61954
ix-t <107044773+ix-t@users.noreply.github.com>
2022-06-14 16:54:00 -0500

    First JS structure part created

--- src/app.js
+++ src/app.js
@@ -1,1 +1,26 @@
+function showTemperature(response) {
+  console.log(response.data);
 
+  let cityElement = document.querySelector("#city");
+  let maxTempElement = document.querySelector("#maxTemp");
+  let minTempElement = document.querySelector("#minTemp");
+  let temperatureElement = document.querySelector("#temperature");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+
+  cityElement.innerHTML = response.data.name;
+  maxTempElement.innerHTML = Math.round(response.data.main.temp_max);
+  minTempElement.innerHTML = Math.round(response.data.main.temp_min);
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let city = "New York";
+let apiKey = "31ee5db17333dd8cbc0df1677e0dc2c4";
+let units = "metric";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
+axios.get(apiUrl).then(showTemperature);
